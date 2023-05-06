5a537ce5b5ce50dfff4087ab177db1c04b80e22c
Rafal Labedz <r.labedz@ravsol.pl>
2023-05-06 17:10:24 +0200

    Add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const titleClickHandler = function(event){
+
+    event.preventDefault();
     console.log('Link was clicked!');
     console.log(event);
+    
   
     /* [DONE]remove class 'active' from all article links  */
  
@@ -10,37 +13,35 @@ const activeLinks = document.querySelectorAll('.titles a.active');
 for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
 }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+
 const clickedElement = this
 console.log('clickedElement:', clickedElement);  
 clickedElement.classList.add('active'); 
 
     /* [DONE] remove class 'active' from all articles */
+
 const activeArticles = document.querySelectorAll('.post');
 for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
 }  
-    /* get 'href' attribute from the clicked link */
+    /* get [DONE] 'href' attribute from the clicked link */
+
+const articleSelector = clickedElement.getAttribute('href');
+console.log(articleSelector);
+
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
   
-    /* find the correct article using the selector (value of 'href' attribute) */
+const targetArticle = document.querySelector(articleSelector);
+ 
+
+    /* [DONE] add class 'active' to the correct article */
+
+targetArticle.classList.add('active'); 
+}
   
-    /* add class 'active' to the correct article */
-  }
+const links = document.querySelectorAll('.titles a');
   
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
+for(let link of links){
     link.addEventListener('click', titleClickHandler);
-  }
-
-
-
-
-
-
-/*
-document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  });
-*/
+}
