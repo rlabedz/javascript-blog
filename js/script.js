cc7112f8ed924034f24156b34f7aef2b06c8cdcf
Rafal Labedz <r.labedz@ravsol.pl>
2023-05-07 11:10:59 +0200

    Add StyleLint

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ const titleClickHandler = function(event){
     event.preventDefault();
     console.log('Link was clicked!');
     console.log(event);
-    
-  
+
+
     /* [DONE]remove class 'active' from all article links  */
- 
+
 const activeLinks = document.querySelectorAll('.titles a.active');
 for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
@@ -16,30 +16,30 @@ for(let activeLink of activeLinks){
     /* [DONE] add class 'active' to the clicked link */
 
 const clickedElement = this
-console.log('clickedElement:', clickedElement);  
-clickedElement.classList.add('active'); 
+console.log('clickedElement:', clickedElement);
+clickedElement.classList.add('active');
 
     /* [DONE] remove class 'active' from all articles */
 
 const activeArticles = document.querySelectorAll('.post');
 for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
-}  
+}
     /* get [DONE] 'href' attribute from the clicked link */
 
 const articleSelector = clickedElement.getAttribute('href');
 console.log(articleSelector);
 
     /* [DONE] find the correct article using the selector (value of 'href' attribute) */
-  
+
 const targetArticle = document.querySelector(articleSelector);
- 
+
 
     /* [DONE] add class 'active' to the correct article */
 
-targetArticle.classList.add('active'); 
+targetArticle.classList.add('active');
 }
-  
+
 
 
 
@@ -95,4 +95,4 @@ function generateTitleLinks(){
 }
 }
 
-generateTitleLinks();     
+generateTitleLinks();
